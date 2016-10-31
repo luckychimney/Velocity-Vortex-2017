8e6860caf4d1dd4a235dcdd8c041c465d53cd64f
Patrick Stephen (PJ) <pjtnt11@gmail.com>
2016-10-31 00:46:12 -0500

    Reformated code

--- ftc_app-2.35/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DefaultTeleOp.java
+++ ftc_app-2.35/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DefaultTeleOp.java
@@ -4,45 +4,63 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 @TeleOp(name = "TeleOp")
-public class DefaultTeleOp extends LinearOpMode {
-    private Robot robot = new Robot();
+public class DefaultTeleOp extends LinearOpMode
+{
+	private Robot robot = new Robot();
 
-    @Override
-    public void runOpMode() throws InterruptedException {
-        waitForStart();
+	@Override
+	public void runOpMode() throws InterruptedException
+	{
+		waitForStart();
 
-        while (opModeIsActive()) {
-            double right_speed;
-            double left_speed;
+		while (opModeIsActive())
+		{
+			double right_speed;
+			double left_speed;
 
-            if (gamepad1.right_bumper) {
-                if (gamepad1.right_stick_y >= 0) {
-                    right_speed = 1;
-                } else {
-                    right_speed = -1;
-                }
+			if (gamepad1.right_bumper)
+			{
+				if (gamepad1.right_stick_y >= 0)
+				{
+					right_speed = 1;
+				}
+				else
+				{
+					right_speed = -1;
+				}
 
-                if (gamepad1.left_stick_y >= 0) {
-                    left_speed = 1;
-                } else {
-                    left_speed = -1;
-                }
-            } else {
-                if (Math.abs(gamepad1.right_stick_y) <= 0.2) {
-                    right_speed = 0;
-                } else {
-                    right_speed = gamepad1.right_stick_y * 0.75;
-                }
+				if (gamepad1.left_stick_y >= 0)
+				{
+					left_speed = 1;
+				}
+				else
+				{
+					left_speed = -1;
+				}
+			}
+			else
+			{
+				if (Math.abs(gamepad1.right_stick_y) <= 0.2)
+				{
+					right_speed = 0;
+				}
+				else
+				{
+					right_speed = gamepad1.right_stick_y * 0.75;
+				}
 
-                if (Math.abs(gamepad1.left_stick_y) <= 0.2) {
-                    left_speed = 0;
-                } else {
-                    left_speed = gamepad1.left_stick_y * 0.75;
-                }
-            }
+				if (Math.abs(gamepad1.left_stick_y) <= 0.2)
+				{
+					left_speed = 0;
+				}
+				else
+				{
+					left_speed = gamepad1.left_stick_y * 0.75;
+				}
+			}
 
-            robot.rightMotor.setPower(right_speed);
-            robot.leftMotor.setPower(left_speed);
-        }
-    }
+			robot.rightMotor.setPower(right_speed);
+			robot.leftMotor.setPower(left_speed);
+		}
+	}
 }
