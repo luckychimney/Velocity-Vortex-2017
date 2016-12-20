922dc83667fe4ddddb13e760532dbfa498916204
Patrick Stephen (PJ) <pjtnt11@gmail.com>
2016-12-20 03:04:12 -0600

    Update App

---
+++ ftc_app-2.4/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/simpleForwardDrive.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+@Autonomous(name = "Simple Forward Drive")
+public class simpleForwardDrive extends Robot
+{
+	@Override
+	public void runOpMode() throws InterruptedException
+	{
+		initializeRobot();
+		waitForGyroCalibration();
+		waitForStart();
+		if (opModeIsActive())
+		{
+			drive(.15, 9999);
+		}
+	}
+}
